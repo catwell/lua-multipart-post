708d6a25ba543d306f658f95512cc62f1e5e9e82
TJ Miller <millert@us.ibm.com>
2019-06-21 01:08:41 -0700

    Use ltn12 chunked streaming to allow for sending large files

--- multipart-post.lua
+++ multipart-post.lua
@@ -1,4 +1,10 @@
 local ltn12 = require "ltn12"
+local url = require "socket.url"
+
+local _M = {}
+
+_M.CHARSET = "UTF-8"
+_M.LANGUAGE = ""
 
 local fmt = function(p, ...)
     if select('#', ...) == 0 then
@@ -10,10 +16,10 @@ local tprintf = function(t, p, ...)
     t[#t+1] = fmt(p, ...)
 end
 
-local append_data = function(r, k, data, extra)
-    tprintf(r, "content-disposition: form-data; name=\"%s\"", k)
+local section_header = function(r, k, extra)
+    tprintf(r, "content-disposition: form-data; name*=%s'%s'%s", _M.CHARSET, _M.LANGUAGE, url.escape(k))
     if extra.filename then
-        tprintf(r, "; filename=\"%s\"", extra.filename)
+        tprintf(r, "; filename*=%s'%s'%s", _M.CHARSET, _M.LANGUAGE, url.escape(extra.filename))
     end
     if extra.content_type then
         tprintf(r, "\r\ncontent-type: %s", extra.content_type)
@@ -25,8 +31,6 @@ local append_data = function(r, k, data, extra)
         )
     end
     tprintf(r, "\r\n\r\n")
-    tprintf(r, data)
-    tprintf(r, "\r\n")
 end
 
 local gen_boundary = function()
@@ -36,44 +40,111 @@ local gen_boundary = function()
   return table.concat(t)
 end
 
-local encode = function(t, boundary)
-    boundary = boundary or gen_boundary()
+local encode = function(r, k, v, boundary)
+    local _t = type(v)
+
+    tprintf(r, "--%s\r\n", boundary)
+    if _t == "string" then
+        section_header(r, k, {})
+    elseif _t == "table" then
+        assert(v.data, "invalid input")
+        local extra = {
+            filename = v.filename or v.name,
+            content_type = v.content_type or v.mimetype
+                or "application/octet-stream",
+            content_transfer_encoding = v.content_transfer_encoding or "binary",
+        }
+        section_header(r, k, extra)
+    else
+        error(string.format("unexpected type %s", _t))
+    end
+end
+
+local encode_source = function(k, v, boundary)
+    r = {}
+    encode(r, k, v, boundary)
+    return ltn12.source.string(table.concat(r))
+end
+
+local data_len = function(d)
+    local _t = type(d)
+
+    if _t == "string" then
+        return string.len(d)
+    elseif _t == "table" then
+        if type(d.data) == "string" then
+            return string.len(d.data)
+        end
+        if d.len then return d.len end
+        error("must provide data length for non-string datatypes")
+    end
+end
+
+local content_length = function(t, boundary)
     local r = {}
-    local _t
-    for k,v in pairs(t) do
-        tprintf(r, "--%s\r\n", boundary)
-        _t = type(v)
-        if _t == "string" then
-            append_data(r, k, v, {})
-        elseif _t == "table" then
-            assert(v.data, "invalid input")
-            local extra = {
-                filename = v.filename or v.name,
-                content_type = v.content_type or v.mimetype
-                    or "application/octet-stream",
-                content_transfer_encoding = v.content_transfer_encoding or "binary",
-            }
-            append_data(r, k, v.data, extra)
-        else error(string.format("unexpected type %s", _t)) end
+    local data_length = 0
+    for k, v in pairs(t) do
+        encode(r, k, v, boundary)
+        data_length = data_length + data_len(v)
+        tprintf(r, "\r\n")
     end
     tprintf(r, "--%s--\r\n", boundary)
-    return table.concat(r), boundary
+
+    return string.len(table.concat(r)) + data_length
 end
+
+local get_data_src = function(v)
+    local _t = type(v)
+    if v.source then
+        return v.source
+    elseif _t == "string" then
+        return ltn12.source.string(v)
+    elseif _t == "table" then
+        _t = type(v.data)
+        if _t == "string" then
+            return ltn12.source.string(v.data)
+        elseif _t == "table" then
+            return ltn12.source.table(v.data)
+        elseif _t == "userdata" then
+            return ltn12.source.file(v.data)
+        elseif _t == "function" then
+            return v.data
+        end
+    end
+    error("invalid input")
+end
+
+local set_ltn12_blksz = function(sz)
+    assert(type(sz) == "number", "set_ltn12_blksz expects a number")
+    ltn12.BLOCKSIZE = sz
+end
+_M.set_ltn12_blksz = set_ltn12_blksz
+
+local source = function(t, boundary)
+    local n = 1
+    local sources = {}
+    for k, v in pairs(t) do
+        sources[n] = encode_source(k, t[k], boundary)
+        sources[n+1] = get_data_src(v)
+        sources[n+2] = ltn12.source.string("\r\n")
+        n = n + 3
+    end
+    sources[n] = ltn12.source.string(string.format("--%s--\r\n", boundary))
+    return ltn12.source.cat(table.unpack(sources))
+end
+_M.source = source
 
 local gen_request = function(t)
     local boundary = gen_boundary()
-    local s = encode(t, boundary)
     return {
         method = "POST",
-        source = ltn12.source.string(s),
+        source = source(t, boundary),
         headers = {
-            ["content-length"] = #s,
-            ["content-type"] = fmt("multipart/form-data; boundary=%s", boundary),
+            ["content-length"] = content_length(t, boundary),
+            ["content-type"] = fmt("multipart/form-data; boundary=\"%s\"", boundary),
         },
     }
 end
+_M.gen_request = gen_request
 
-return {
-    encode = encode,
-    gen_request = gen_request,
-}
+return _M
--- multipart-post.test.lua
+++ multipart-post.test.lua
@@ -13,6 +13,13 @@ do -- Find a JSON parser
     assert(ok and J, "no JSON parser found :(")
 end
 
+local file = io.open("testfile.tmp", "w+")
+file:write("file data")
+file:flush()
+file:seek("set", 0)
+local fsz = file:seek("end")
+file:seek("set", 0)
+
 local T = cwtest.new()
 
 T:start("tests")
@@ -20,18 +27,21 @@ T:start("tests")
 local r = {}
 local rq = mp{
     myfile = {name = "myfilename", data = "some data"},
+    diskfile = {name = "diskfilename", data = file, len = fsz},
+    ltn12file =  {name = "ltn12filename", data = ltn12.source.string("ltn12 data") , len = string.len("ltn12 data")},
     foo = "bar",
 }
+
 rq.url = "http://httpbin.org/post"
 rq.sink = ltn12.sink.table(r)
 local b, c, h = H(rq)
 
 T:eq( c, 200 )
-
 r = J(table.concat(r))
 
-T:eq( r.files, {myfile = "some data"} )
+T:eq( r.files, { myfile="some data", diskfile="file data", ltn12file="ltn12 data"} )
 T:eq( r.form, {foo = "bar"} )
 
 T:done()
 
+os.remove("testfile.tmp")
