e23791d9fce762ae3c63141a14d5d042d42c5d32
Pierre Chapuis <catwell@archlinux.us>
2019-07-14 12:30:42 +0200

    fix use of global variable

--- multipart-post.lua
+++ multipart-post.lua
@@ -61,7 +61,7 @@ local encode = function(r, k, v, boundary)
 end
 
 local encode_source = function(k, v, boundary)
-    r = {}
+    local r = {}
     encode(r, k, v, boundary)
     return ltn12.source.string(table.concat(r))
 end
